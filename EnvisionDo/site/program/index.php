46b3a21bab5f955dc0dcf0c6bc5b00731f0ac5a6
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2012-04-22 13:25:09 -0500

    Initializing the repository with all existing EnvisionDo Files

---
+++ EnvisionDo/site/program/index.php
@@ -0,0 +1,120 @@
+<?php 
+
+require_once("../functions/hello.php");
+	
+	$page = "Program";
+
+	echo pageTemplate($page);
+	echo pageHeader($page); ?>
+</div>
+<div class="content">
+	<div class="page">
+<div class="sidebar">
+<h3>Features</h3>
+<ol>
+<li><h4>Scope</h4>
+<p>Most conferences focus in a specific industry, and students have limited exposure to other potential change models.  For instance, to alleviate poverty, one could donate to charity causes, start a social enterprise, publish academic journals that propose solutions, be the CEO of a company that set aside a significant corporate social responsibility budget, or create a documentary that brings awareness to the issue. Instead of focusing in just one method of creating change, our conference will provide students who have the ambition to be change catalysts the exposure to multiple change platforms. We hope that this diverse spectrum could enable young leaders to better envision and examine avenues through which they could achieve the most impact.</p></li>
+
+<li><h4>50/50 model </h4>
+<p>We believe action and execution should play as important a role as lectures. We will tie the conference closely with our incubator program to support, track, and help execute projects that are generated during or after the conference. We hope that the strong emphasis on networking and bonding during the conference enables them to generate sustainable initiatives that they will commit to after the conference. </p></li>
+</ol>
+
+	</div>
+
+<div class="nonbar">
+<h3>The first EnvisionDo conference took place on May 21-22, 2011.</h3>
+<p>EnvisionDo Conference brings together student leaders to generate ideas and form teams to launch sustainable initiatives. We firmly believe that the best change model is for people to pick the profession they are passionate it, and then integrate social value into their career. Therefore, we define initiatives broadly. It could be a tech startup, a conference, a non-profit organization, a documentary, or anything other medium you can think of.</p>
+
+<p>EnvisionDo Conference is a unique experience that supports this model in three steps:</p>
+
+<ol>
+
+<li>Generate ideas
+<p>We provide participants an all round exposure to change agents in the society through our speakers and EnvisionDo Bazaar.</p>
+
+<ul>
+<li class="no-bullet"><b>Keynote Speaker:</b> Dr. Peter M. Slowe</h3>
+
+<img src="../images/slowe.jpg" alt="Dr. Peter M. Slowe" />
+ 
+<p>Dr. Peter M. Slowe is the Chair and Director of Projects Abroad, the global leader in short-term, overseas, volunteer work placements. Head-quartered in the UK and with recruitment offices across the globe, its projects span 27 countries and cover a wide range of disciplines including teaching, care, conservation & environment, medicine & healthcare, sports, community projects, business, law & human rights and journalism.  Projects are designed specifically for the needs of the communities they serve, giving volunteers the very best experience of volunteering abroad.</p>
+
+<p>Projects Abroad was founded as a programme to help students to travel and work outside term-time. Projects Abroad has now sent over 28,000 volunteers around the world, working on over 500 projects.  Dr. Slowe says:</p>
+
+<blockquote>“What drives Projects Abroad is its combination of successful business with effective philanthropy.  It has managed to create a profitable service that depends for its success on doing good works in developing countries.  The better it performs for poor people, for sick children, for saving wildlife, and so on, the more satisfied its volunteers, who are its only source of income, become and the more profit it then makes for re-investment.  It has created a unique virtuous circle.”</blockquote>
+ 
+<p>Dr. Slowe received his Doctor of Philosophy degree in Geography from the University of Oxford and his Bachelor of Science degree from the University of Bristol. Previously, he was a Senior Lecturer in Geography at the University of Chichester and the Chief Executive of the Co-operative Enterprise Centre.</p>
+</li>
+
+<li class="no-bullet">Donald Laackman, President, Harold Washington College
+
+<img src="../images/laackman.jpg" alt="Donald Laackman" />
+
+<p>Don Laackman has just been appointed the next President of Harold Washington College, the downtown campus of the City Colleges of Chicago (CCC) seven colleges system.</p>
+
+<p>Before the appointment, Mr. Laackman was the principal at Civic Consulting Alliance (CCA), where he managed CCA's Workforce and Education program areas.  He had led several workforce system change initiatives, including the creation of a new workforce board overseeing $360 million in workforce funding in Chicago, a strategy for improving Career and Technical Education for 25,000 Chicago Public School high school students, and the creation of several advisory councils comprised of business, civic and community leaders to advise CCC on its Reinvention initiative.</p>
+
+<p>Mr. Laackman previously served as a managing director at Accenture, a global technology, consulting and outsourcing firm. Over a twenty-three year career, he led strategic, change, and information technology (IT) solution initiatives while providing client management, professional development, business unit management, profit and loss delivery, contract negotiations, and sales.</p>
+
+<p>Mr. Laackman received his AB in Politics, Economics, Rhetoric and Law and a Master in Public Policy from the University of Chicago. He now serves on the Visiting Committee for the Dean of the College at the University of Chicago and the Visiting Committee for the Harris School of Public Policy at the University of Chicago.</p>
+</li>
+
+<li>EnvisionDo Bazaar
+
+<p>EnvisionDo Bazaar aims to connect students with local startups, corporations, and non-profits that have created services or products that add social value to the community. Through our Bazaar, you can interact closely with founders of startups, representatives from corporations with strong CSR (corporate social responsibility), and leaders of non-profit organizations. This is also a great chance to get connected with ventures you want to work for or get involved with in the future.</p>
+</li>
+</ul>
+</li>
+
+<li>Form teams
+<p>One feature that distinguishes EnvisionDo the most is our focus in the friendships and bondings created through the conference. This enables close collaborations on ventures and initiatives during and after the conference. It is also a good means of networking for budding leaders of the entrepreneurship and social domain. Our goal is to bond leaders and doers from diverse background and talents for immediate creations of projects.</p>
+
+<p>The programs to facilitate this process are:</p>
+
+<ul>
+<li>Civic Consulting Summit
+ 
+<p>The Civic Consulting Summit serves to bolster the EnvisionDo experience by having participants bond and work together in a team-based setting.  Participants will be assigned to teams on the basis of their applications.  Each team will be tasked with finding a way mitigate the negative externalities of a business upon the community in an innovative and sustainable manner, all within the boundaries of an assigned case.  Teams will focus on the innovation, sustainability, and effectiveness of their solution.</p></li>
+ 
+<li>International Night
+ 
+<p>After a day of distinguished speakers and thought-provoking workshops, conference participants will relax and bond with fellow EnvisionDoers through a fun and light-hearted multicultural dance workshop. During registration, participants will select their top 2 choices for the dance in which they are interested. Dance instructors will teach 1-2 minutes of choreography to their respective groups in 45 minutes. Each group then gets to showcase its multicultural flair to all groups during the conference dinner. This will be a fun and memorable evening for all participants!</p></li>
+</ul>
+
+<p>The dance workshops are:</p>
+
+<ul>
+<li>Bhangra
+ 
+<p>Bhangra is a form of dance and music that originated in the Punjab region and is based on music from Punjabi folk drums and folk singing. It is seen in the West as an expression of South Asian culture as a whole.</p></li>
+
+<li>Bollywood
+ 
+<p>The dancing in Bollywood films is primarily modeled on Indian dance: classical and historic dance styles and folk dances. In modern films, Indian dance elements often blend with Western dance styles (such as those seen in music videos or musicals).</p></li>
+ 
+<li>Latin dance/Salsa
+ 
+<p>Latin dance includes a wide range of dances originating in Latin America, Puerto Rico, and Cuba. Salsa, in particular, has origins in Cuba as a major original American meeting point of Spanish (European) and African cultures.</p></li>
+ 
+<li>Modern Korean Pop
+ 
+<p>Korean pop is a musical genre consisting of electronic, hip hop, pop, and R&B music originating in South Korea. “K-pop” has grown into a popular subculture among teenagers and young adults throughout Asia, and Korean groups and singers have adopted a unique form of dance in their music videos and shows.</p></li>
+ 
+<li>Swing Dance
+ 
+<p>Swing dance developed concurrently with the swing style of jazz music in the 1920s to ‘50s. Swing jazz features the syncopated timing associated with African American and West African music and dance, and the “Lindy Hop” is the most popular swing dance.</p></li>
+</ul>
+</li>
+
+<li>Launch initiatives
+
+<p>There will be opportunities during the conference for participants to discuss and generate projects they could develop in collaboration with some other participants. This part of the conference will tie into the EnvisionDo incubator program, which is a year-long support network for founders of ventures and initiatives. All projects that are generated through our conference will be supported and monitored by the EnvisionDo Board. We also have a vast amount of mentors from a diverse background to guide and support project leaders.</p>
+
+</li>
+</ol>
+
+</div>
+</div>
+	
+
+<? echo pageFooter($page); ?>
